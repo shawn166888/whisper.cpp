07c841797c9abe5d8f99dc2c461814ec577797ed
shawn@miar <shawn@miar.com>
2024-04-12 22:58:53 +0800

    final init

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
-# model
+
+#init
 sudo chmod 777 /mnt
 cp models/download-ggml-model.sh /mnt/
-cd /mnt/
-if test -e "ggml-medium.bin"
-    then   echo "ggml-medium.bins exist"
+cd /mnt
+if [  -f "ggml-medium.bin"  ]; then
+        echo "file_exist"
 else
-    source /download-ggml-model.sh medium
-# input
-wget --no-check-certificate -O 11.wav todo
+        echo "file not exist"
+        source /download-ggml-model.sh medium
+fi
+
+#
+wget --no-check-certificate -O 11.wav "todo here"
