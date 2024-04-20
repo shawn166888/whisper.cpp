b848f901625de26beb39f82bfcab3cc107784c13
shawn@miar <shawn@miar.com>
2024-04-20 20:11:54 +0800

    move lock file location

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 # build\bin\main -m /mnt/ggml-medium.bin -f /mnt/11.wav -l ja -tr -on 10
-./main -m /mnt/ggml-medium.bin -f /mnt/11.wav -of /mnt/11.srt -l ja -tr
+# ./main -m /mnt/ggml-medium.bin -f /mnt/11.wav -of /mnt/11.srt -l ja -tr
+./main -m /mnt/ggml-medium.bin -f /mnt/11.wav -of ~/11.srt -l ja -tr
