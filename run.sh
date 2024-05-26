67f1ee56e1221f6339995c8eb584c6df8248cf78
shawn <shawn@desk2>
2024-05-26 23:24:43 +0800

    add sh run

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # build\bin\main -m /mnt/ggml-medium.bin -f /mnt/11.wav -l ja -tr -on 10
 # ./main -m /mnt/ggml-medium.bin -f /mnt/11.wav -of /mnt/11.srt -l ja -tr
-./main -m /mnt/ggml-medium.bin -f /mnt/11.wav -of ~/11.srt -l ja -tr
+# ./main -m /mnt/ggml-medium.bin -f /mnt/11.wav -of ~/11.srt -l ja -tr
+./main -m /mnt/d/lib/ggml-tiny.bin -f /mnt/d/lib/pk.wav -of /mnt/d/lib/pk.srt -l ja -tr
